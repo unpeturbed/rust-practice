7e87d19f2c1d0396ce92e19251e4fcdef10d78d8
unpeturbed <ayodeledrust@gmail.com>
2024-09-19 17:50:12 +0100

    Format String

--- 01-basic-rust/01_hello_world/hello_world_05/src/main.rs
+++ 01-basic-rust/01_hello_world/hello_world_05/src/main.rs
@@ -1,6 +1,11 @@
 // Format a string with multiple variables and print it.
 
 fn main() {
-    println!("Hello, world!");
-}
+    let name: String = "Unpeturbed".into();
+    let surn_name = String::from("Petur");
+    let occupation: &str = "Dev".into();
+    let location: &str = "Metaverse";
+
+    println!("My name is {} {}, and I am a {} who hails from the {}", name, surn_name, occupation, location);
+} 
 
