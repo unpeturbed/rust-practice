f3a9a9ec8f13726bdeaab65dbcef8750c95f0223
unpeturbed <ayodeledrust@gmail.com>
2024-09-19 14:07:00 +0100

    Take user input and print

--- 01-basic-rust/01_hello_world/hello_world_02/src/main.rs
+++ 01-basic-rust/01_hello_world/hello_world_02/src/main.rs
@@ -1,5 +1,13 @@
 // Modify the program to take user input and print "Hello, [name]!".
-
+use std::io::{self};
 fn main() {
-    println!("Hello, world!");
+    
+    let mut name = String::new();
+    println!("Please enter your name: ");
+    io::stdin()
+        .read_line(&mut name)
+        .expect("Unable to read user input");
+
+    //Trim the newline from the input    
+    println!("Hello, {}", name.trim());
 }
