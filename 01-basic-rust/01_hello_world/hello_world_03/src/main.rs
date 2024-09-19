b94936fd89b98515ce4d64b5c8c0b51ec05a57c9
unpeturbed <ayodeledrust@gmail.com>
2024-09-19 17:30:13 +0100

    Greeting based on time of the day

--- 01-basic-rust/01_hello_world/hello_world_03/src/main.rs
+++ 01-basic-rust/01_hello_world/hello_world_03/src/main.rs
@@ -1,6 +1,19 @@
 // Create a program that prints a greeting based on the time of day.
-
+use std::time::{SystemTime, UNIX_EPOCH};
 
 fn main() {
-    println!("Hello, world!");
+    // Get the current time
+    let duration_since_epoch = SystemTime::now().duration_since(UNIX_EPOCH).expect("Maybe time went backwards");
+
+    let current_hr = (duration_since_epoch.as_secs() /3600) % 24;
+
+    //let current_min = (duration_since_epoch.as_secs() / 60) % 60;
+    //println!("{}hr : {}mins", current_hr, current_min);
+
+    match current_hr {
+        0..=11 => println!("Good Morning"),
+        12..=17 => println!("Good Afternoon"),
+        18..=23 => println!("Good Evening"),
+        _ => println!("Hello"),
+    }
 }
