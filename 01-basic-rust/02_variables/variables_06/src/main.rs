c88bc4d3d928614533063b7c916652082a52bf2c
unpeturbed <ayodeledrust@gmail.com>
2024-09-28 18:08:10 +0100

    Upload

--- 01-basic-rust/02_variables/variables_06/src/main.rs
+++ 01-basic-rust/02_variables/variables_06/src/main.rs
@@ -1,6 +1,10 @@
 // Convert a string to an integer and print the result.
 
 fn main() {
-    println!("Hello, world!");
+
+    let num_str: &str = "10";
+    let num: i32 = num_str.parse().unwrap();
+
+    println!("num = {num}");
 }
 
