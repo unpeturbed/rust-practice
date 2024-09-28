d83fdb4156d93dac04941f9b96c7e58db7fcd198
unpeturbed <ayodeledrust@gmail.com>
2024-09-28 11:54:44 +0100

    Shadow a vairaible and print its values

--- 01-basic-rust/02_variables/variables_04/src/main.rs
+++ 01-basic-rust/02_variables/variables_04/src/main.rs
@@ -1,6 +1,13 @@
 // Shadow a variable and print both values.
 
 fn main() {
-    println!("Hello, world!");
+    let input = 5;
+    println!("Before shadowed by inner scope, input = {input}");
+    {
+        let input = "Universe";
+        println!("After shadowed by inner scope, input = {input}");
+    }
+    let input = 45.3;
+    println!("After shadowed by outer scope, input = {input}");
 }
 
