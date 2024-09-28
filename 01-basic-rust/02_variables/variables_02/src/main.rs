11928d51e452c5fa4cf2fcd046509896e4ec7c68
unpeturbed <ayodeledrust@gmail.com>
2024-09-28 11:53:16 +0100

    Declare a Constant and prints its value

--- 01-basic-rust/02_variables/variables_02/src/main.rs
+++ 01-basic-rust/02_variables/variables_02/src/main.rs
@@ -1,6 +1,8 @@
 // Declare a constant and print its value.
 
 fn main() {
-    println!("Hello, world!");
+    const HIGHEST_POINT_ON_EARTH: u32 = 8848;
+
+    println!("Mount Everest's peak is the highest point on earth: {HIGHEST_POINT_ON_EARTH}m");
 }
 
--- 01-basic-rust/02_variables/variables_03/src/main.rs
+++ 01-basic-rust/02_variables/variables_03/src/main.rs
@@ -1,6 +1,11 @@
 // Create a mutable variable and change its value inside a loop.
 
 fn main() {
-    println!("Hello, world!");
+    let mut number = 5;
+    while number != 0 {
+        println!("{}", number);
+        number -= 1;
+    }
+    println!("KABOOM!");
 }
 
